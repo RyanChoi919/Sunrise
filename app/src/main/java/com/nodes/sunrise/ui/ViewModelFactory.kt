157bb655730541fa1158a851f57dad451944a662
ryanchoi919 <choiyh919@gmail.com>
2022-08-03 01:07:35 +0900

    minor fix

--- app/src/main/java/com/nodes/sunrise/ui/ViewModelFactory.kt
+++ app/src/main/java/com/nodes/sunrise/ui/ViewModelFactory.kt
@@ -3,6 +3,8 @@ package com.nodes.sunrise.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.nodes.sunrise.db.AppRepository
+import com.nodes.sunrise.ui.challenge.check.ChallengeCheckViewModel
+import com.nodes.sunrise.ui.challenge.select.ChallengeSelectViewModel
 import com.nodes.sunrise.ui.home.HomeViewModel
 
 class ViewModelFactory(private val repository: AppRepository) : ViewModelProvider.Factory {
@@ -11,6 +13,9 @@ class ViewModelFactory(private val repository: AppRepository) : ViewModelProvide
         when {
             modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
                 return HomeViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(ChallengeSelectViewModel::class.java) -> {
+                return ChallengeSelectViewModel(repository) as T
             }
         }
         throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.simpleName}")
--- app/src/main/java/com/nodes/sunrise/ui/challenge/select/ChallengeSelectFragment.kt
+++ app/src/main/java/com/nodes/sunrise/ui/challenge/select/ChallengeSelectFragment.kt
@@ -1,24 +1,22 @@
 package com.nodes.sunrise.ui.challenge.select
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.nodes.sunrise.BaseApplication
 import com.nodes.sunrise.R
+import com.nodes.sunrise.ui.BaseFragment
 import com.nodes.sunrise.ui.ViewModelFactory
-import com.nodes.sunrise.ui.home.HomeViewModel
 
-class ChallengeSelectFragment : Fragment() {
+class ChallengeSelectFragment : BaseFragment() {
 
     companion object {
         fun newInstance() = ChallengeSelectFragment()
     }
 
-    private val viewModel: HomeViewModel by viewModels {
+    private val viewModel: ChallengeSelectViewModel by viewModels {
         val repository = (requireActivity().application as BaseApplication).repository
         ViewModelFactory(repository)
     }
