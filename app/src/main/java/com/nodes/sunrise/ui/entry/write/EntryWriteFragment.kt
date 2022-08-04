033373599d408f3ba7e221efd42f8c12367eeb3e
ryanchoi919 <choiyh919@gmail.com>
2022-08-04 22:14:56 +0900

    implement save menu function

--- app/src/main/java/com/nodes/sunrise/ui/entry/write/EntryWriteFragment.kt
+++ app/src/main/java/com/nodes/sunrise/ui/entry/write/EntryWriteFragment.kt
@@ -1,12 +1,15 @@
 package com.nodes.sunrise.ui.entry.write
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import com.nodes.sunrise.BaseApplication
+import com.nodes.sunrise.R
 import com.nodes.sunrise.databinding.FragmentEntryWriteBinding
 import com.nodes.sunrise.ui.ViewModelFactory
 
@@ -26,16 +29,45 @@ class EntryWriteFragment : Fragment() {
     ): View {
 
         _binding = FragmentEntryWriteBinding.inflate(inflater, container, false)
+
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        createMenu()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun createMenu() {
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menu.clear()
+                menuInflater.inflate(R.menu.frag_entry_write_menu_create, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.frag_entry_write_menu_save -> {
+                        with(binding) {
+                            val title = fragEntryWriteETTitle.text.toString()
+                            val content = fragEntryWriteETContent.text.toString()
+                            viewModel.write(title, content)
+                            findNavController().popBackStack()
+                            true
+                        }
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
--- app/src/main/java/com/nodes/sunrise/ui/entry/write/EntryWriteViewModel.kt
+++ app/src/main/java/com/nodes/sunrise/ui/entry/write/EntryWriteViewModel.kt
@@ -1,8 +1,38 @@
 package com.nodes.sunrise.ui.entry.write
 
+import androidx.lifecycle.viewModelScope
 import com.nodes.sunrise.db.AppRepository
+import com.nodes.sunrise.db.entity.Entry
 import com.nodes.sunrise.ui.BaseViewModel
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 class EntryWriteViewModel(val repository: AppRepository) : BaseViewModel(repository) {
+
+    var dateTime: LocalDateTime = LocalDateTime.now()
+    lateinit var entry: Entry
+
+    /**
+     * 새로운 entry를 작성한 후 데이터를 저장하는 메소드
+     */
+    fun write(title: String, content: String) {
+        entry = Entry(0, dateTime, title, true, content)
+
+        viewModelScope.launch {
+            insert(entry)
+        }
+    }
+
+    /**
+     * 기존 entry를 수정한 후 수정된 데이터를 저장하는 메소드
+     */
+    fun modify(title: String, content: String) {
+        entry.title = title
+        entry.content = content
+
+        viewModelScope.launch {
+            insert(entry)
+        }
+    }
 
 }
